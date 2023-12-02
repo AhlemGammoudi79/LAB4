8eee4a31f274e3e901e3c21b6e7519e188ab2939
ahlem <ahlem.gammoudi@yess-consulting.com>
2023-12-02 19:43:04 +0100

    commit

--- sources/etudiant.py
+++ sources/etudiant.py
@@ -25,7 +25,7 @@ def rechercher_etudiant_par_nom(nom):
 
 # Étape 4 : Fonction pour afficher la liste complète des étudiants
 def afficher_liste_etudiants():
-    print("Liste des étudiants en MP2L /ahlem :")
+    print("Liste des étudiants en MP2L /ahlem gammoudi :")
     for etudiant in liste_etudiants:
         print(f"Nom: {etudiant.nom}, Age: {etudiant.age}, Matricule: {etudiant.matricule}")
 
